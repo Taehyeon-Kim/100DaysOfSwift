f3d493fa986af9ba622fba3c27ed0fd1efc039ed
Taehyeon-Kim <taehyk528@gmail.com>
2021-04-01 22:01:32 +0900

    feat: 19일차 학습내용 - Project 2 세팅
    - Assets
    - x2, x3 이미지
    - UIButton
    - AutoLayout
    - CALayer (테두리)

---
+++ day16-22_iOS시작/Project2/Project2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by taehy.k on 2021/04/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
