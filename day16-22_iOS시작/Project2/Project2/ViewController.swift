f6d4c0e37754d9caa89611f1f6da50b5addd677b
Taehyeon-Kim <taehyk528@gmail.com>
2021-04-01 22:02:31 +0900

    feat: 19일차 내용 나머지
    - 누락된 내용들

--- day16-22_iOS시작/Project2/Project2/ViewController.swift
+++ day16-22_iOS시작/Project2/Project2/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    var countries = [String]()
+    var score = 0
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // 배열도 문자열처럼 덧셈기호를 이용해서 원소를 추가할 수 있다.
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        // 테두리가 CALayer에 속하기 때문에 UIColor로만 반환하면 값을 사용할 수가 없다.
+        // 따라서, 뒤에 cgColor를 붙여준다.
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        // 함수 호출
+        askQuestion()
     }
 
+    func askQuestion() {
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+    }
 
+    // Core Animation 타입인 CALayer 사용
 }
 
